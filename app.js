16a5c3a8a0df8367fa02dcf100f69a05cbb71022
MilosTodorovic= <xxthejaxx@gmail.com>
2020-03-29 20:54:57 +0200

    Added support for edditing and updating employees

--- app.js
+++ app.js
@@ -69,6 +69,17 @@ app.get('/edit/:zap_redni_broj', (req, res) => {
     });
 });
 
+app.post('/updateEmployees', (req, res) => {
+    const zap_redni_broj = req.body.zap_redni_broj;
+    let sql = "UPDATE employees SET TIP_UST = '" + req.body.tip_ust + "' , VU_IDENTIFIKATOR = '" + req.body.vu_identifikator + "' , ZAP_REDNI_BROJ = '" 
+              + req.body.zap_redni_broj + "' , ZAP_PREZIME = '" + req.body.zap_prezime + "' , ZAP_SREDNJE_SLOVO = '" + req.body.zap_srednje_slovo + 
+              "' , ZAP_IME = '" + req.body.zap_ime + "' WHERE ZAP_REDNI_BROJ =  " + zap_redni_broj; 
+    let query = connection.query(sql, (err, results) => {
+        if(err) throw err;
+        res.redirect('/');
+    });
+});
+
 // Server listening
 app.listen(3000, () => {
     console.log('Server is running at port 3000');
